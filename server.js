837cc0015a2f232a6d719e6aed50e7cb06b05f68
Nanda Kisor Chary <kisors@it.prodigygroupindia.com>
2026-06-21 09:34:27 +0530

    fix: simplify fallback routing in express server.js and lock build configurations in vite

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, 'dist')))
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
 const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
