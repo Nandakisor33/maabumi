a55733257725b65635843074d0bf208834899932
Nanda Kisor Chary <kisors@it.prodigygroupindia.com>
2026-06-21 08:56:20 +0530

    fix: update preview and dev commands to accept host 0.0.0.0 directly

--- vite.config.js
+++ vite.config.js
@@ -1,51 +1,39 @@
 import { defineConfig, transformWithOxc } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
-import { resolve } from 'path'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   base: '/',
-  optimizeDeps: {
-    rolldownOptions: {
-      moduleTypes: {
-        '.js': 'jsx',
-      },
-    },
-  },
+
   server: {
-    host: '0.0.0.0',
-    port: process.env.PORT ? parseInt(process.env.PORT) : 8080,
-    strictPort: true,
+    host: '0.0.0.0'
   },
+
   preview: {
-    host: '0.0.0.0',
-    port: process.env.PORT ? parseInt(process.env.PORT) : 8080,
-    strictPort: true,
+    host: '0.0.0.0'
   },
+
   plugins: [
     react(),
+
     {
-      name: "transform-jsx-in-js",
-      enforce: "pre",
+      name: 'transform-jsx-in-js',
+      enforce: 'pre',
+
       async transform(code, id) {
         if (!id.match(/src\/.*\.js$/)) {
-          return null;
+          return null
         }
+
         return await transformWithOxc(code, id, {
-          lang: "jsx",
-        });
-      },
+          lang: 'jsx'
+        })
+      }
     }
   ],
+
   build: {
-    rolldownOptions: {
-      input: {
-        main: resolve('index.html'),
-      },
-      moduleTypes: {
-        '.js': 'jsx',
-      },
-    },
-  },
-}))
+    outDir: 'dist',
+    emptyOutDir: true
+  }
+})
